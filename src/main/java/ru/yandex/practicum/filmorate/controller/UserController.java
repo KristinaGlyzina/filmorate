cdc36cbb9115fa995179f9de1799323890607271
Kristina <glyzinakristina@gmail.com>
2023-07-08 23:18:50 +0600

    внесены новые корректировки

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.ResponseEntity;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
@@ -14,38 +14,18 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/users")
 @Slf4j
 public class UserController {
-    private Map<Integer, User> userMap = new HashMap<>();
     private int nextId = 1;
+    private Map<Integer, User> userMap = new HashMap<>();
 
-    @PostMapping(value = "/users")
-    public User createUser(@RequestBody User user) throws ValidationException {
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) throws ValidationException {
+        validateUser(user);
+
         if (userMap.values().stream().anyMatch(u -> u.getEmail().equals(user.getEmail()))) {
-            throw new ValidationException("User with email already exists.");
-        }
-
-        if (userMap.values().stream().anyMatch(u -> u.getLogin().equals(user.getLogin()))) {
-            throw new ValidationException("User with login already exists.");
-        }
-
-        if (user.getEmail() == null || user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
-            log.error("Invalid email: {}.", user.getEmail());
-            throw new ValidationException("Invalid email.");
-        }
-
-        if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
-            log.error("Invalid login: {}.", user.getLogin());
-            throw new ValidationException("Invalid login.");
-        }
-
-        if (user.getName() == null || user.getName().isEmpty()) {
-            user.setName(user.getLogin());
-        }
-        LocalDate currentDate = LocalDate.now();
-        if (user.getBirthday() != null && user.getBirthday().isAfter(currentDate)) {
-            log.error("Invalid birth date: {}.", user.getBirthday());
-            throw new ValidationException("Invalid birth date.");
+            throw new ValidationException("User with the same email already exists.");
         }
 
         int id = getNextId();
@@ -54,17 +34,20 @@ public class UserController {
 
         log.info("User created: {}.", user);
 
-        return user;
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
-    @PutMapping(value = "/users")
+    @PutMapping
     public ResponseEntity<User> updateUser(@RequestBody User updatedUser) throws ValidationException {
         int id = updatedUser.getId();
         User existingUser = userMap.get(id);
         if (existingUser == null) {
-            log.error("User not found with id: {}.", id);
+            log.error("User not found: {}.", id);
             throw new ValidationException("User not found.");
         }
+
+        validateUser(updatedUser);
+
         existingUser.setEmail(updatedUser.getEmail());
         existingUser.setLogin(updatedUser.getLogin());
         existingUser.setName(updatedUser.getName());
@@ -75,14 +58,33 @@ public class UserController {
         return ResponseEntity.ok(existingUser);
     }
 
-    @GetMapping(value = "/users")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers() throws ValidationException {
         List<User> users = new ArrayList<>(userMap.values());
-        if (users == null || users.isEmpty()) {
+        if (users.isEmpty()) {
             log.error("List of users is empty.");
             throw new ValidationException("List of users is empty.");
         }
         return ResponseEntity.ok(users);
+    }
+
+    private void validateUser(User user) throws ValidationException {
+        if (user.getEmail() == null || user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Invalid email format.");
+        }
+
+        if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Invalid login format.");
+        }
+
+        if (user.getName() == null || user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        if (user.getBirthday() != null && user.getBirthday().isAfter(currentDate)) {
+            throw new ValidationException("Invalid birthday date.");
+        }
     }
 
     private int getNextId() {
