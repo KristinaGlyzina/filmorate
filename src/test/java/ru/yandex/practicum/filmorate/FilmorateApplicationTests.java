1c7ff9af11dd3886069bddedea030187e6f77f65
kristinaglyzina <glyzinakristina@gmail.com>
2023-08-24 10:04:20 +0600

    внесены корректировки 5 по checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -26,6 +26,7 @@ import java.util.HashSet;
 import java.util.List;
 
 @SpringBootTest
+@SuppressWarnings("CheckStyle")
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public
